eecd5792287ec34605dea84eba1ef5e7ac12eba5
Jackie Huang <jackie.huang@windriver.com>
2019-11-07 15:24:58 +0800

    build_oran_nxp.sh: temp change for nxp
    use internal git repo since the nxp BSP is not yet published
    
    Signed-off-by: Jackie Huang <jackie.huang@windriver.com>
    Change-Id: Ifab15d1212fb4696469a654a141c163e9c84373f

--- scripts/build_oran_nxp.sh
+++ scripts/build_oran_nxp.sh
@@ -76,12 +76,12 @@ echo_info "For oran layer source: ${SRC_ORAN_DIR}"
 echo_info "For build project: ${PRJ_BUILD_DIR}"
 
 # Clone the source of WRLinux BASE 10.18 from github and setup
-RUN_CMD="git clone --branch WRLINUX_10_18_BASE git://github.com/WindRiver-Labs/wrlinux-x.git"
+RUN_CMD="git clone --branch WRLINUX_10_18_BASE git://pek-git.wrs.com/git/wrlinux-x"
 echo_cmd "Cloning wrlinux 1018 source from github:"
 cd ${SRC_WRL_DIR}
 ${RUN_CMD}
 
-RUN_CMD="./wrlinux-x/setup.sh --machines intel-x86-64 --layers meta-cloud-services"
+RUN_CMD="./wrlinux-x/setup.sh --machines nxp-lx2xxx --layers meta-cloud-services --dl-layers"
 echo_cmd "Setup wrlinux build project:"
 ${RUN_CMD}
 
@@ -95,10 +95,14 @@ if cd ${SCRIPTS_DIR} && git rev-parse --is-inside-work-tree > /dev/null 2>&1; th
 else
     echo_info "Cloning oran layer:"
     cd ${SRC_ORAN_DIR}
-    RUN_CMD="git clone https://gerrit.o-ran-sc.org/r/pti/rtp"
+    RUN_CMD="git clone --branch jackiehjm/master-nxp-20191107 http://stash.wrs.com/scm/~jhuang0/o-ran-pti-rtp.git rtp"
     echo_cmd "Cloing with:"
     ${RUN_CMD}
 fi
+
+# Apply meta patches
+cd ${SRC_WRL_DIR}/layers/nxp-lx2xxx/
+git am ${SRC_ORAN_DIR}/rtp/scripts/meta-patches/nxp-lx2xxx/0001-Revert-nxp-lx2xxx-remove-preempt-rt-related-file.patch
 
 # Source the build env
 cd ${SRC_WRL_DIR}
@@ -119,7 +123,17 @@ cat << EOF >> conf/local.conf
 ########################
 DISTRO = "oran-inf"
 BB_NO_NETWORK = '0'
-WRTEMPLATE += "feature/oran-host-rt-tune"
+WRTEMPLATE += " \\
+    feature/oran-host-rt-tune \\
+    feature/kubernetes \\
+"
+
+# For container image
+WR_APP_CONTAINER_APP = "rt-tests"
+
+# Disable multilib fo nxp-lx2xxx
+MULTILIBS_nxp-lx2xxx = ""
+DEFAULTTUNE_virtclass-multilib-lib32_nxp-lx2xxx = ""
 EOF
 
 if [ "${EXTRA_CONF}" != "" ] && [ -f "${EXTRA_CONF}" ]; then
@@ -132,5 +146,9 @@ TIMESTAMP=`date +"%Y%m%d_%H%M%S"`
 RUN_CMD="bitbake ${DRYRUN} oran-image-inf-host"
 echo_cmd "Build the oran-image-inf-host image"
 bitbake ${DRYRUN} oran-image-inf-host 2>&1|tee logs/bitbake_oran-image-inf-host_${TIMESTAMP}.log
+
+RUN_CMD="bitbake ${DRYRUN} wr-app-container"
+echo_cmd "Build the wr-app-container image"
+bitbake ${DRYRUN} wr-app-container 2>&1|tee logs/bitbake_wr-app-container_${TIMESTAMP}.log
 
 echo_info "Build succeeded, you can get the image in ${PRJ_BUILD_DIR}/tmp-glibc/deploy/images/intel-x86-64/oran-image-inf-host-intel-x86-64.iso"
